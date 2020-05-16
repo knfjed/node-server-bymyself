bd19be5a45ae1029637a62b148da17ea4ff84b12
knfjed <knfjed@gmail.com>
2020-05-16 12:34:31 +0900

    add replace obj

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 
 var myServer = http.createServer((request, response) => {
   fs.readFile("./index.html", "UTF-8", (error, data) => {
+    var content = data
+      .replace(/dummy-title/g, "タイトルです")
+      .replace(/dummy-contents/, "コンテンツです");
+
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(data);
+    response.write(content);
     response.end();
   });
 });

66f2e8ca94717e749f94acc58e30899f838377e0
knfjed <knfjed@gmail.com>
2020-05-17 08:24:45 +0900

    get cookie data

--- server.js
+++ server.js
@@ -58,6 +58,9 @@ function response_index(request, response) {
     // データ受信終了時の処理
     request.on("end", () => {
       data = qs.parse(body);
+
+      // クッキーの保存
+      setCookie("msg", data.msg, response);
       write_index(request, response);
     });
   } else {
@@ -65,13 +68,15 @@ function response_index(request, response) {
   }
 }
 
-// indexの表示の作成
+// indexページの作成
 function write_index(request, response) {
   var msg = "＊伝言を表示します";
+  var cookie_data = getCookie("msg", request);
   var text = ejs.render(index_page, {
     title: "index",
     content: msg,
     data: data,
+    cookie_data: cookie_data,
   });
 
   response.writeHead(200, { "Content-Type": "text/html" });
@@ -79,16 +84,36 @@ function write_index(request, response) {
   response.end();
 }
 
-// otherのアクセス処理
-function response_other(request, response) {
-  var msg = "これはotherのページです";
-  var text = ejs.render(other_page, {
-    title: "Other",
-    content: msg,
-    data: data,
-    filename: "data_item",
-  });
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(text);
-  response.end();
+// クッキーの値の設定
+function setCookie(key, value, response) {
+  var cookie = escape(value);
+  response.setHeader("Set-Cookie", [key + "=" + cookie]);
 }
+
+// クッキーの値を取得
+function getCookie(key, request) {
+  var cookie_data =
+    request.headers.cookie != undefined ? request.headers.cookie : "";
+  var data = cookie_data.split(";");
+  for (var i in data) {
+    if (data[i].trim().startsWith(key + "=")) {
+      var result = data[i].trim().substring(key.length + 1);
+      return unescape(result);
+    }
+  }
+  return "";
+}
+
+// // otherのアクセス処理
+// function response_other(request, response) {
+//   var msg = "これはotherのページです";
+//   var text = ejs.render(other_page, {
+//     title: "Other",
+//     content: msg,
+//     data: data,
+//     filename: "data_item",
+//   });
+//   response.writeHead(200, { "Content-Type": "text/html" });
+//   response.write(text);
+//   response.end();
+// }

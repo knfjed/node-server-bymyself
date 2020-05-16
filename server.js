15a9863121d77a918c59200004f2e6c0e63557a9
knfjed <knfjed@gmail.com>
2020-05-16 14:51:20 +0900

    add readFileSync style.css

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
+const url = require("url");
 
 const index_page = fs.readFileSync("./index.ejs", "utf-8");
+const style_css = fs.readFileSync("./style.css");
 
 const server = http.createServer(getFromClient);
 
 function getFromClient(request, response) {
-  const text = ejs.render(index_page, {
-    title: "Index",
-    content: "これはテンプレートを使ったサンプルです",
-  });
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(text);
-  response.end();
+  const url_parts = url.parse(request.url);
+  switch (url_parts.pathname) {
+    case "/":
+      const text = ejs.render(index_page, {
+        title: "Index",
+        content: "これはテンプレートを使ったサンプルです",
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(text);
+      response.end();
+      break;
+
+    case "/style.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      response.write(style_css);
+      response.end();
+      break;
+
+    default:
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end();
+      break;
+  }
 }
 
 server.listen(3050);

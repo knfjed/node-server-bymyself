600310e38e11fe5094312f3a23d2a41916de5e6a
knfjed <knfjed@gmail.com>
2020-05-16 15:15:14 +0900

    add other page by ejs

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const ejs = require("ejs");
 const url = require("url");
 
 const index_page = fs.readFileSync("./index.ejs", "utf-8");
-const style_css = fs.readFileSync("./style.css");
+const other_page = fs.readFileSync("./other.ejs", "utf-8");
+const style_css = fs.readFileSync("./style.css", "utf-8");
 
 const server = http.createServer(getFromClient);
 
@@ -12,7 +13,7 @@ function getFromClient(request, response) {
   const url_parts = url.parse(request.url);
   switch (url_parts.pathname) {
     case "/":
-      const text = ejs.render(index_page, {
+      var text = ejs.render(index_page, {
         title: "Index",
         content: "これはテンプレートを使ったサンプルです",
       });
@@ -27,9 +28,18 @@ function getFromClient(request, response) {
       response.end();
       break;
 
+    case "/other":
+      var text = ejs.render(other_page, {
+        title: "other",
+        content: "other page です",
+      });
+      response.write(text);
+      response.end();
+      break;
+
     default:
       response.writeHead(200, { "Content-Type": "text/plain" });
-      response.end();
+      response.end("no page....sorry");
       break;
   }
 }

976349d187338f322fa3c20c139b305feb4996fd
knfjed <knfjed@gmail.com>
2020-05-16 19:51:26 +0900

    add commentouts

--- server.js
+++ server.js
@@ -60,11 +60,14 @@ function response_other(request, response) {
     var body = "";
 
     // データ受信時のイベント処理
+    // データを受け取ったら、都度追加し、変数bodyで保管
     request.on("data", (data) => {
       body += data;
     });
 
     // データ受信終了時のイベント処理
+    // データを全て受け取ったら、最後にパースして値として取り出す
+    // endイベントは全てのデータを受け取った後に発火するので、引数（渡されるデータ）はない
     request.on("end", () => {
       var post_data = qs.parse(body);
       msg += "あなたは" + post_data.msg + "と入力しました";

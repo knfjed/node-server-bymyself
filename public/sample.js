6957093687ca24a0230b4d45fa556d612244b932
knfjed <knfjed@gmail.com>
2020-05-16 12:11:26 +0900

    create server by http request

--- public/sample.js
+++
@@ -1,2 +0,0 @@
-const text = document.getElementById("hoge");
-text.innerText = "Hi!";
--- server.js
+++ server.js
@@ -1,36 +1,13 @@
-// const http = require("http");
+const http = require("http");
+const fs = require("fs");
 
-// // サーバーの立ち上げ
-// const server = http.createServer();
-// const myServer = server.on("request", function (req, res) {
-//   res.writeHead(200, { "Contents-Type": "text/plain" });
-//   res.write("hello world!!!!!!");
-//   res.end();
-// });
-
-// myServer.listen(8000);
-// console.log("server Listening.....");
-
-const express = require("express");
-const app = express();
-// パス指定
-const path = require("path");
-
-// ポートで待ち受け
-app.listen(8000, () => {
-  console.log("server listening....");
+var myServer = http.createServer((request, response) => {
+  fs.readFile("./index.html", "UTF-8", (error, data) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
 });
 
-// expressでルーティング
-//1) publicフォルダまでのパス文字列を作成
-//2) 1.の文字列をexpress.staticメソッドに渡して静的ファイルをルーティングするミドルウェアを作成
-//3) 2.のミドルウェアをapp.useメソッドに渡して、8080へのアクセス時に利用されるようにする
-
-app.use(express.static(path.join(__dirname, "public")));
-
-// その他のリクエストに対するエラー
-
-app.use((req, res) => {
-  res.sendStatus(404);
-  console.log("404 error desu");
-});
+myServer.listen(3050);
+console.log("server listening.....");
